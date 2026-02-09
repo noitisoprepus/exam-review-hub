360a527120f06bd0fc162ff5be8adf59202e7c54
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-02-09 15:12:17 +0000

    feat: Install and import ConfigModule

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [UsersService],
 })
 export class AuthModule {}
