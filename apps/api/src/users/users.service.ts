f2a3ce78acb9d7759bfa0c6d536e6833644b69ad
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-02-21 13:14:39 +0000

    feat: Implement user CRUD functions in the UsersService

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { Prisma, User } from 'prisma/generated/prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+        deleted: false,
+      },
+    });
+  }
+
+  async findByUsername(username: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        username,
+        deleted: false,
+      },
+    });
+  }
+
+  async findMany(
+    skip?: number,
+    take?: number,
+    cursor?: Prisma.UserWhereUniqueInput,
+    where?: Prisma.UserWhereInput,
+    orderBy?: Prisma.UserOrderByWithRelationInput,
+  ): Promise<User[]> {
+    return this.prisma.user.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async update(id: string, data: Prisma.UserUpdateInput): Promise<User> {
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string): Promise<User> {
+    return this.prisma.user.update({
+      where: { id },
+      data: {
+        deleted: true,
+        deletedAt: new Date(),
+      },
+    });
+  }
+}
