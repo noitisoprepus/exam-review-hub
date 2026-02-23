c81da5a19687b72c8f0f06e856345e934ef22301
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-02-23 16:14:53 +0000

    feat: Create PasswordService

---
+++ apps/api/src/auth/password.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+
+@Injectable()
+export class PasswordService {
+  constructor() {}
+
+  validatePassword(password: string, hashedPassword: string): Promise<boolean> {
+    return argon2.verify(hashedPassword, password);
+  }
+
+  hashPassword(password: string): Promise<string> {
+    return argon2.hash(password);
+  }
+}
