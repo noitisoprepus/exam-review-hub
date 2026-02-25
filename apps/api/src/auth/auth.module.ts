21734a9bdf8913a8209df30f09a00df0ef8ce2fb
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-02-26 00:12:49 +0800

    feat: Set up imports and providers for the AuthModule

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from 'src/users/users.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { RefreshTokenService } from './refresh-token.service';
+import { PasswordService } from './password.service';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, PasswordService, JwtStrategy, RefreshTokenService],
   controllers: [AuthController],
-  imports: [UsersService],
+  imports: [
+    UsersService,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
 })
 export class AuthModule {}
