c971ea2ab05b765cf2252be30b3ce9e1a5e74cd4
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-03-02 23:12:28 +0800

    feat: Update AuthModule to include PrismaService and correct UsersModule import

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { RefreshTokenService } from './refresh-token.service';
 import { PasswordService } from './password.service';
+import { UsersModule } from 'src/users/users.module';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [AuthService, PasswordService, JwtStrategy, RefreshTokenService],
+  providers: [
+    AuthService,
+    PasswordService,
+    JwtStrategy,
+    RefreshTokenService,
+    PrismaService,
+  ],
   controllers: [AuthController],
   imports: [
-    UsersService,
+    UsersModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
