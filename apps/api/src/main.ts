5b39eaa4685b3fb514382da8e9cf7cf3cb6280f9
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-03-04 22:49:26 +0800

    feat: Declare ConfigModule as a global module

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  ConfigModule.forRoot({
+    isGlobal: true,
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
