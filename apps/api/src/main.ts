dabdec30b601e4241efeddafa1053da8b0b32f28
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-03-08 20:27:05 +0800

    feat: Configure swagger in the api application

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Exam Review Hub API')
+    .setDescription('The exam-review-hub API description')
+    .setVersion('0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
