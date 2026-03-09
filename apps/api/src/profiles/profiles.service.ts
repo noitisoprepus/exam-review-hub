ec8e7aa456ff24d0ed04a18fad0489c6e9cd7735
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-03-09 23:51:30 +0800

    feat: Implement user and creator profile methods in ProfilesService

--- apps/api/src/profiles/profiles.service.ts
+++ apps/api/src/profiles/profiles.service.ts
@@ -1,11 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import {
+  CreatorProfile,
+  Prisma,
+  UserProfile,
+} from 'prisma/generated/prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class ProfilesService {
-  constructor(
-    private readonly prismaService: PrismaService,
-    private readonly usersService: UsersService,
-  ) {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  // UserProfile
+
+  async findUserProfileByUserId(userId: string): Promise<UserProfile | null> {
+    return this.prismaService.userProfile.findUnique({
+      where: { userId },
+    });
+  }
+
+  async createUserProfile(
+    data: Prisma.UserProfileCreateInput,
+  ): Promise<UserProfile> {
+    return this.prismaService.userProfile.create({ data });
+  }
+
+  async updateUserProfile(
+    id: string,
+    data: Prisma.UserProfileUpdateInput,
+  ): Promise<UserProfile> {
+    return this.prismaService.userProfile.update({
+      where: { id },
+      data,
+    });
+  }
+
+  // CreatorProfile
+
+  findCreatorProfileByUserId(userId: string): Promise<CreatorProfile | null> {
+    return this.prismaService.creatorProfile.findUnique({
+      where: { userId },
+    });
+  }
+
+  async createCreatorProfile(
+    data: Prisma.CreatorProfileCreateInput,
+  ): Promise<CreatorProfile> {
+    return this.prismaService.creatorProfile.create({ data });
+  }
+
+  async updateCreatorProfile(
+    id: string,
+    data: Prisma.CreatorProfileUpdateInput,
+  ): Promise<CreatorProfile> {
+    return this.prismaService.creatorProfile.update({
+      where: { id },
+      data,
+    });
+  }
 }
