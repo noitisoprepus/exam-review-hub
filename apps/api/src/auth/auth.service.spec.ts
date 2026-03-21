ee5a5d8debc0d1d68e9a0d22bf8e9e84d6377879
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-03-21 23:40:04 +0800

    refactor: Clean up AuthService test setup

--- apps/api/src/auth/auth.service.spec.ts
+++ apps/api/src/auth/auth.service.spec.ts
@@ -1,61 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { UsersService } from 'src/users/users.service';
-import { PasswordService } from './password.service';
-import { JwtService } from '@nestjs/jwt';
-import { RefreshTokenService } from './refresh-token.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
-  let usersService: jest.Mocked<UsersService>;
-  let passwordService: jest.Mocked<PasswordService>;
-  let jwtService: jest.Mocked<JwtService>;
-  let refreshTokenService: jest.Mocked<RefreshTokenService>;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthService,
-        {
-          provide: UsersService,
-          useValue: {
-            findById: jest.fn(),
-            findByEmail: jest.fn(),
-            create: jest.fn(),
-          },
-        },
-        {
-          provide: PasswordService,
-          useValue: {
-            hashPassword: jest.fn(),
-            validatePassword: jest.fn(),
-          },
-        },
-        {
-          provide: JwtService,
-          useValue: {
-            sign: jest.fn(),
-          },
-        },
-        {
-          provide: RefreshTokenService,
-          useValue: {
-            findValid: jest.fn(),
-            create: jest.fn(),
-            revoke: jest.fn(),
-            revokeAllUserTokens: jest.fn(),
-          },
-        },
-      ],
+      providers: [AuthService],
     }).compile();
 
     service = module.get(AuthService);
-
-    usersService = module.get(UsersService);
-    passwordService = module.get(PasswordService);
-    jwtService = module.get(JwtService);
-    refreshTokenService = module.get(RefreshTokenService);
   });
 
   it('should be defined', () => {
