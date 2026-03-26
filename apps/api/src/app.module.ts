b0acec14ad4bf1a09d95b6cd3a79fbfa67979907
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-03-26 22:42:09 +0800

    feat: Scaffold module, service, and controller for Questions and (exam) Attempts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,6 +6,8 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProfilesModule } from './profiles/profiles.module';
 import { ExamsModule } from './exams/exams.module';
+import { QuestionsModule } from './questions/questions.module';
+import { AttemptsModule } from './attempts/attempts.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { ExamsModule } from './exams/exams.module';
     UsersModule,
     ProfilesModule,
     ExamsModule,
+    QuestionsModule,
+    AttemptsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
