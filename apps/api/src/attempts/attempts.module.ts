7c06a8368cdeff21a4fc06e77959ce7c90f804a7
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2026-05-27 03:27:54 +0800

    feat: Add PrismaModule import to AttemptsModule

--- apps/api/src/attempts/attempts.module.ts
+++ apps/api/src/attempts/attempts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AttemptsService } from './attempts.service';
 import { AttemptsController } from './attempts.controller';
+import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [AttemptsService],
   controllers: [AttemptsController],
+  exports: [AttemptsService],
 })
 export class AttemptsModule {}
